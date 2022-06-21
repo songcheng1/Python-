f34bef6aa1756993e639b16f4d01533aefc797ab
songcheng <39392285+songcheng1@users.noreply.github.com>
2022-06-21 18:43:27 +0800

    Update small_red_book_sign.py

--- small_red_book_sign.py
+++ small_red_book_sign.py
@@ -110,3 +110,16 @@ if __name__ == '__main__':
         "t": ""
       }
       print(sign_with_query_items(params))
+
+# 算法三
+
+import hashlib
+
+x_sign = "X"
+# api = "/fe_api/burdock/v2/user/keyInfo/${userId}"
+api = "/fe_api/burdock/v2/user/keyInfo/6011212c000000000101e7f4"
+m = hashlib.md5()
+m.update((api + "WSUDD").encode())
+sign = x_sign + m.hexdigest()
+print(sign)
+    
